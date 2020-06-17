206becc21054744a59fb2ee3eccffd50d7fec0fe
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-17 12:57:22 -0300

    Creates component commad line

---
+++ src/app/creates-component-command-line/creates-component-command-line.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creates-component-command-line',
+  templateUrl: './creates-component-command-line.component.html',
+  styleUrls: ['./creates-component-command-line.component.scss']
+})
+export class CreatesComponentCommandLineComponent {}

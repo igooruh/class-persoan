eda1193dd2e63e66d6f0db8488bfdc3e9e236063
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-17 12:40:59 -0300

    Creates my first component

---
+++ src/app/myFirstComponent/my-first.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-first-component',
+    template: `<p>Meu primeiro component com Angular</p>`
+})
+export class MyFirstComponent {}

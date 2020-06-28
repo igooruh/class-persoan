73d7223da6520e66755f2cf0261d613b49e0416c
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-27 22:21:47 -0300

    Inject provider data binding service

--- src/app/dataBinding/data-binding.module.ts
+++ src/app/dataBinding/data-binding.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DataBindingComponent } from './data-binding.component';
+import { DataBindingService } from './data-binding.service';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,9 @@ import { DataBindingComponent } from './data-binding.component';
   ],
   exports: [
     DataBindingComponent
+  ],
+  providers: [
+    DataBindingService
   ]
 })
 export class DataBindingModule { }
